395ddc97c89ba2e0f6f2b8a04d2ccf49dce1123f
Vincent Bergeron <vincent_bergeron98@hotmail.com>
2024-07-04 06:01:23 -0400

    WIP

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -10,8 +10,9 @@ it('returns user age based on birthdate', function () {
     travelTo(Carbon::parse('april 1st 2024'));
 
     $user = User::factory()
-        ->has(Profile::factory()->state(['birthdate' => Carbon::parse('march 7th 1998')]))
-        ->create();
+        ->make();
+
+    $user->setRelation('profile', Profile::factory()->make(['birthdate' => Carbon::parse('march 7th 1998')]));
 
     expect($user->profile->age)->toBe(26);
 });
